c4690e2c656b7e7f709cd34afe235d6fc25a185b
Jade570 <44695194+Jade570@users.noreply.github.com>
2020-06-07 11:32:45 +0900

    Update soundgenerate.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
-let skyboxShader;
+let objectShader, skyboxShader;
 let img = [];
 let cup;
-let gl; // WebGL pointer
-let tex, texLoc;
+let gl1, gl2; // WebGL pointer
+let objtex, objtexloc, tex, texLoc;
+let cnv;
 
 function preload() {
   skyboxShader = loadShader('shader/skybox.vert', 'shader/skybox.frag');
+  objectShader = loadShader('shader/object.vert', 'shader/object.frag');
+
   // load six cubemap textures
   img[0] = loadImage("assets/sample/right.jpg");
   img[1] = loadImage("assets/sample/left.jpg");
@@ -17,96 +20,34 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+
   noStroke();
   textureMode(NORMAL);
   setupCubeMap();
+  //setupobjecttexture();
+
+
+
+
+
+
+  ///////////////SOUND_GENERATE//////////////
+  setupOscillator();
+  startOscillator();
+
 }
 
 function draw() {
   clear();
   orbitControl();
+  //rendercup();
   renderSkyBox(); // draw skybox
-}
 
 
-function setupCubeMap() {
-  gl = this._renderer.GL;
-  tex = gl.createTexture();
-  gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_CUBE_MAP, tex);
-  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X, 0, gl.RGBA, gl.RGBA,
-    gl.UNSIGNED_BYTE, img[0].canvas);
-  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 0, gl.RGBA, gl.RGBA,
-    gl.UNSIGNED_BYTE, img[1].canvas);
-  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Y, 0, gl.RGBA, gl.RGBA,
-    gl.UNSIGNED_BYTE, img[2].canvas);
-  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, gl.RGBA, gl.RGBA,
-    gl.UNSIGNED_BYTE, img[3].canvas);
-  gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Z, 0, gl.RGBA, gl.RGBA,
-    gl.UNSIGNED_BYTE, img[4].canvas);
-  gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, gl.RGBA, gl.RGBA,
-    gl.UNSIGNED_BYTE, img[5].canvas);
-  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER,
-    gl.LINEAR);
-}
 
-function renderSkyBox() {
-  gl = this._renderer.GL;
-  tex = gl.createTexture();
-  shader(skyboxShader);
-  // set uniform variable for cubemap texture
-  gl.useProgram(skyboxShader._glProgram);
-  texLoc = gl.getUniformLocation(skyboxShader._glProgram, "cubeMap");
-  gl.uniform1i(texLoc, 0);
-
-  gl.depthFunc(gl.LEQUAL);
-  push();
-  // right
-  beginShape();
-  vertex(1, -1, -1, 0, 0);
-  vertex(1, 1, -1, 0, 1);
-  vertex(1, 1, 1, 1, 1);
-  vertex(1, -1, 1, 1, 0);
-  endShape();
-  //left
-  beginShape();
-  vertex(-1, -1, 1, 0, 0);
-  vertex(-1, 1, 1, 0, 1);
-  vertex(-1, 1, -1, 1, 1);
-  vertex(-1, -1, -1, 1, 0);
-  endShape();
-  // top
-  beginShape();
-  vertex(-1, -1, 1, 0, 0);
-  vertex(-1, -1, -1, 0, 1);
-  vertex(1, -1, -1, 1, 1);
-  vertex(1, -1, 1, 1, 0);
-  endShape();
-  //bottom
-  beginShape();
-  vertex(-1, 1, -1, 0, 0);
-  vertex(-1, 1, 1, 0, 1);
-  vertex(1, 1, 1, 1, 1);
-  vertex(1, 1, -1, 1, 0);
-  endShape();
-  //front
-  beginShape();
-  vertex(-1, -1, -1, 0, 0);
-  vertex(-1, 1, -1, 0, 1);
-  vertex(1, 1, -1, 1, 1);
-  vertex(1, -1, -1, 1, 0);
-  endShape();
-  // back
-  beginShape();
-  vertex(1, -1, 1, 0, 0);
-  vertex(1, 1, 1, 0, 1);
-  vertex(-1, 1, 1, 1, 1);
-  vertex(-1, -1, 1, 1, 0);
-  endShape();
-  pop();
-  // return z-depth test back to default mode
-  gl.depthFunc(gl.LESS);
-  resetShader();
+  /////////////////SOUND_GENERATE/////////////////
+  updateOscillator();
+  stopOscillator();
+  playOscillator();
 }
--- soundgenerate.js
+++ soundgenerate.js
@@ -2,10 +2,7 @@ let osc_sin, osc_tri, osc_saw, osc_square;
 let freq, amp, scene, playing;
 let sel;
 
-function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  cnv.mousePressed(playOscillator);
-
+function setupOscillator(){
   osc_sin = new p5.Oscillator('sine');
   osc_tri = new p5.Oscillator('triangle');
   osc_saw = new p5.Oscillator('sawtooth');
@@ -19,103 +16,102 @@ function setup() {
   sel.option('4');
 }
 
-function draw() {
-  background(220)
+function startOscillator() {
+cnv.mousePressed(function(){
+  switch (scene) {
+    case '1':
+      osc_sin.start();
+      osc_saw.start();
+      break;
+
+    case '2':
+      osc_square.start();
+      osc_saw.start();
+      break;
+
+    case '3':
+      osc_tri.start();
+      osc_square.start();
+      break;
+
+    case '4':
+      osc_saw.start();
+      osc_tri.start();
+      break;
+  }
+
+  playing = true;
+});
+}
+
+function stopOscillator() {
+  cnv.mouseReleased(function(){
+    switch (scene) {
+      case '1':
+        osc_sin.amp(0, 0.5);
+        osc_saw.amp(0, 0.5);
+        break;
+
+      case '2':
+        osc_square.amp(0, 0.5);
+        osc_saw.amp(0, 0.5);
+        break;
+
+      case '3':
+        osc_tri.amp(0, 0.5);
+        osc_square.amp(0, 0.5);
+        break;
+
+      case '4':
+        osc_saw.amp(0, 0.5);
+        osc_tri.amp(0, 0.5);
+        break;
+    }
+    playing = false;
+  });
+}
+
+function playOscillator(){
+  if (playing) {
+    switch (scene) {
+      case '1':
+        osc_sin.freq(int(freq), 0.1);
+        osc_sin.amp(amp, 0.1);
+
+        osc_saw.freq(freq, 0.1);
+        osc_saw.amp(1 - amp, 0.1);
+        break;
+
+      case '2':
+        osc_saw.freq(int(freq), 0.1);
+        osc_saw.amp(amp, 0.1);
+
+        osc_square.freq(freq, 0.1);
+        osc_square.amp(1 - amp, 0.1);
+        break;
+
+      case '3':
+        osc_square.freq(int(freq), 0.1);
+        osc_square.amp(amp, 0.1);
+
+        osc_tri.freq(freq, 0.1);
+        osc_tri.amp(1 - amp, 0.1);
+        break;
+
+      case '4':
+        osc_tri.freq(int(freq), 0.1);
+        osc_tri.amp(amp, 0.1);
+
+        osc_saw.freq(freq, 0.1);
+        osc_saw.amp(1 - amp, 0.1);
+        break;
+    }
+  }
+}
+
+function updateOscillator() {
   freq = constrain(map(mouseY, height, 0, 100, 500), 100, 500);
   amp = constrain(map(mouseX, 0, width, 0, 1), 0, 1);
 
   scene = sel.value();
-
-  if (playing) {
-    if (scene == '1') {
-
-      osc_sin.freq(int(freq), 0.1);
-      osc_sin.amp(amp, 0.1);
-
-      osc_saw.freq(freq, 0.1);
-      osc_saw.amp(1 - amp, 0.1);
-    }
-
-    if (scene == '2') {
-
-      osc_saw.freq(int(freq), 0.1);
-      osc_saw.amp(amp, 0.1);
-
-      osc_square.freq(freq, 0.1);
-      osc_square.amp(1 - amp, 0.1);
-    }
-
-    if (scene == '3') {
-
-      osc_square.freq(int(freq), 0.1);
-      osc_square.amp(amp, 0.1);
-
-      osc_tri.freq(freq, 0.1);
-      osc_tri.amp(1 - amp, 0.1);
-    }
-
-    if (scene == '4') {
-
-      osc_tri.freq(int(freq), 0.1);
-      osc_tri.amp(amp, 0.1);
-
-      osc_saw.freq(freq, 0.1);
-      osc_saw.amp(1 - amp, 0.1);
-    }
-  }
-}
-
-function playOscillator() {
-  // starting an oscillator on a user gesture will enable audio
-  // in browsers that have a strict autoplay policy.
-  // See also: userStartAudio();
-
-  if (scene == '1') {
-    osc_sin.start();
-    osc_saw.start();
-  }
-
-  if (scene == '2') {
-    osc_square.start();
-    osc_saw.start();
-  }
-
-  if (scene == '3') {
-    osc_tri.start();
-    osc_square.start();
-  }
-
-  if (scene == '4') {
-    osc_saw.start();
-    osc_tri.start();
-  }
-
-  playing = true;
-}
-
-function mouseReleased() {
-  // ramp amplitude to 0 over 0.5 seconds
-
-  if (scene == '1') {
-    osc_sin.amp(0, 0.5);
-    osc_saw.amp(0, 0.5);
-  }
-
-  if (scene == '2') {
-    osc_square.amp(0, 0.5);
-    osc_saw.amp(0, 0.5);
-  }
-
-  if (scene == '3') {
-    osc_tri.amp(0, 0.5);
-    osc_square.amp(0, 0.5);
-  }
-
-  if (scene == '4') {
-    osc_saw.amp(0, 0.5);
-    osc_tri.amp(0, 0.5);
-  }
-
-  // osc.amp(0, 0.5);
-  playing = false;
 }
